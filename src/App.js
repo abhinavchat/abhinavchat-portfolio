ca7d56ae3b0edf128f112c443f89cba891a20d48
Abhinav Chaturvedi <abhinav.c@live.in>
2022-11-18 14:04:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <header className={classes["App-header"]}>
+        <i>Abhinav Chaturvedi</i>
+        <span>Coming Soon!</span>
+        <span>Work in progress!</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
